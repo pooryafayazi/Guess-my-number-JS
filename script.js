65166c02d71e68d9d7619b2484b396b284e98eb4
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-05-01 21:25:27 +0330

    08-082

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉🎊 Correct Number! 🎊🎉';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.score').textContent = 19;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
